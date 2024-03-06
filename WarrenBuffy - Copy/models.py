b7a028d952f628f1feeceaddaea76c277004907d
jheikkinen <56417363+jheikkinen@users.noreply.github.com>
2024-03-05 22:03:46 -0500

    Delete WarrenBuffy - Copy directory

--- WarrenBuffy - Copy/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class MyModel(models.Model):
-    phone = models.CharField(max_length=10)
-    city = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'user'
-        app_label = 'WarrenBuffy'
